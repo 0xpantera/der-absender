771b6c62f9ff6960e65f8cffaa58b8d215f6d974
0xpantera <0xpantera@proton.me>
2025-05-15 16:23:06 +0200

    add sepolia to chains

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, mainnet } from "wagmi/chains";
+import { anvil, mainnet, sepolia } from "wagmi/chains";
 
 export default getDefaultConfig({
   appName: "DerAbsender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [anvil, mainnet],
+  chains: [anvil, mainnet, sepolia],
   ssr: false,
 });
